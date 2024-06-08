377e60faf714f7054baad0ae67db7a94895f0997
Mathias Bevers <mathiasbevers@outlook.com>
2024-06-08 10:38:47 +0200

    Wrote the first unit test.
    - created a simple sum function.
    - added a if statement to check the sum function in the test method.
    - ran the test with wrong and right inputs to check if the test is behaving as expected.
    - naming conventions need to be updated.

--- PersonalPortfolio4/UnitTest1.cs
+++ PersonalPortfolio4/UnitTest1.cs
@@ -1,6 +1,14 @@
+using System;
+
 namespace PersonalPortfolio4;
 
 public class UnitTest1
 {
-    [Fact] public void Test1() { }
+    [Fact]
+    public void Test1()
+    {
+        if (Sum(2, 2) != 4) { throw new Exception(); }
+    }
+
+    private int Sum(int left, int right) => left + right;
 }
