f1424f2f240768c0c082f815dcdc967d5d969063
Mathias Bevers <mathiasbevers@outlook.com>
2024-06-22 15:18:03 +0200

    Wrote the core of the initalization and run methods.
    - all the classes which inherit from the 'IDay' name space are put into a list and sorted by date.
    - the classes are called in order to init and solve star one and star two.
    - unit tests are not passed since the stars are not yet implemented.

--- domain/AdventOfCode.cs
+++ domain/AdventOfCode.cs
@@ -1,6 +1,46 @@
-﻿namespace domain;
+﻿using System.Reflection;
+
+namespace domain;
 
 public class AdventOfCode
 {
+    public int runningDaysCount { get; private set; }
+    public string fileDirectory { get; private set; }
     
+    private readonly List<IDay> days;
+    
+    //TODO: save output data to file 
+
+    public AdventOfCode(string fileDirectory)
+    {
+        this.fileDirectory = fileDirectory;
+        days = new List<IDay>();
+
+        foreach (Type type in Assembly.GetExecutingAssembly().GetTypes())
+        {
+            if (!type.GetInterfaces().Contains(typeof(IDay))) { continue; }
+
+            if (Activator.CreateInstance(type) is not IDay day)
+            {
+                throw new InvalidCastException($"Something went wrong with activating the {type.Name} class as a day");
+            }
+
+            days.Add(day);
+        }
+        
+        days.Sort((a,b) => a.date.CompareTo(b.date));
+        runningDaysCount = days.Count;
+    }
+
+
+    public void Run()
+    {
+        foreach (IDay day in days)
+        {
+            day.Initialize(fileDirectory);
+
+            string starOne = day.StarOne();
+            string starTwo = day.StarTwo();
+        }
+    }
 }
